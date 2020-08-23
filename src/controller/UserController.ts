f21e6be361cb21976ee339b512c49b406ca8db26
Daniel Nascimento Pimentel <dnprocks@yahoo.com.br>
2020-08-23 00:39:14 -0300

    login route with generating JWT

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -3,7 +3,8 @@ import { Request, Response } from 'express';
 import { Controller, Post } from '@overnightjs/core';
 import { User } from '@src/entities/user';
 import { UserRepository } from '@src/repositories/implementation/UserRepository';
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import AuthService from '@src/util/AuthService';
 
 @Controller('user')
 export class UserController {
@@ -23,5 +24,21 @@ export class UserController {
         response.status(500).send({ error: 'Internal Server Error' });
       }
     }
+  }
+
+  @Post('authenticate')
+  public async authenticate(req: Request, res: Response): Promise<Response> {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(401).send({ error: 'Try verifying your email address.'});
+    }
+    if (
+      !(await AuthService.comparePasswords(req.body.password, user.password))
+    ) {
+      return res.status(401).send({ error: 'Password does not match!'});
+    }
+    const token = AuthService.generateToken(user.toJSON());
+
+    return res.send({ ...user.toJSON(), ...{ token } });
   }
 }
